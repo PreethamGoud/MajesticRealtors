724debac14cc83d4ed0d9033c21de8ec91387f37
dsouzall <dsouzall@mail.uc.edu>
2023-11-07 19:05:06 -0500

    Importing of library

--- Houses.cs
+++ Houses.cs
@@ -3,6 +3,8 @@
     using System;
     using System.Collections.Generic;
     using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
 
     public partial class Houses
     {
