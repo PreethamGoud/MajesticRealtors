f8017acae091ce1d33679144d8dc9f10cad57131
Preetham Goud <preethamsureshgoud@gmail.com>
2023-11-28 17:33:20 -0500

    Swagger

--- Program.cs
+++ Program.cs
@@ -16,6 +16,30 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Specimens API",
+        Description = "A look at specimens that are thirsty at the Cincinnati Zoo and Botanical Garden",
+        TermsOfService = new Uri("https://plantplaces.com/privacy.shtml"),
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Contact PlantPlaces",
+            Url = new Uri("https://plantplaces.com/privacy.shtml")
+        },
+        License = new Microsoft.OpenApi.Models.OpenApiLicense
+        {
+            Name = "Plant Places License",
+            Url = new Uri("https://plantplaces.com/privacy.shtml")
+        }
+    });
+
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,6 +56,13 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseRouting();
 
